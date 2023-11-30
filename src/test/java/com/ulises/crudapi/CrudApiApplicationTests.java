6616fc49980175d72472a2d4e035ab831138ca80
ulisestrujillo <ciborgdev@gmail.com>
2023-11-30 06:14:53 -0700

    se agregan pruebas unitarias para empleadocontroller

--- src/test/java/com/ulises/crudapi/CrudApiApplicationTests.java
+++ src/test/java/com/ulises/crudapi/CrudApiApplicationTests.java
@@ -1,13 +1,35 @@
 package com.ulises.crudapi;
 
+import com.ulises.crudapi.entity.Empleado;
+import com.ulises.crudapi.model.EmpleadoRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CrudApiApplicationTests {
+	@Autowired
+	TestRestTemplate testRestTemplate;
 
 	@Test
-	void contextLoads() {
+	void debeRegresarForbiddenAlGrabarEmpleado() {
+		var request = new HashMap<String, Object>();
+		request.put("nombre", "Delia");
+		request.put("apellido", "García");
+		request.put("puesto", "Líder");
+
+		ResponseEntity<String> response =
+				testRestTemplate.postForEntity ("/empleado/add", request, String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
 	}
 
 }
